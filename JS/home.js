1a5380beeddf71b85f68dde54056bd70c48c50b5
Simranbist2005 <seemranbist@gmail.com>
2026-06-04 16:00:07 +0545

    Added JS for Home page

--- JS/home.js
+++ JS/home.js
@@ -7,3 +7,28 @@ const buttons = document.querySelectorAll(".card_btn");
 buttons.forEach((button) => {
   button.addEventListener("click", cartFunction);
 });
+
+const subscribe_btn = document.querySelector(".subscribe-btn");
+const input = document.querySelector("#email");
+const message = document.querySelector("#message");
+
+const validate = () => {
+  //get the user email from local storage
+  const savedEmail = localStorage.getItem("subscriberEmail");
+  message.style.display = "block"; //shows popup message
+  if (input.value.trim() === "" || !input.value.includes("@")) {
+    message.innerText = "Please enter valid email";
+  } else if (savedEmail === input.value) {
+    message.innerText = "Already Subscribed";
+  } else {
+    //save the user email to local storage
+    localStorage.setItem("subscriberEmail", input.value);
+    message.innerText = "Thanks for subscribing";
+    input.value = "";
+  }
+  // to automatically hide the text after 3 secs
+  setTimeout(() => {
+    message.style.display = "none";
+  }, 2000);
+};
+subscribe_btn.addEventListener("click", validate);
