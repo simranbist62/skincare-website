2ec3bbb5e6c982ae35514affe3728b62b40356bd
Simranbist2005 <seemranbist@gmail.com>
2026-06-09 10:09:52 +0545

    changed the name of card class to product-card

---
+++ JS/cart.js
@@ -0,0 +1,2 @@
+// localStorage.getItems("cartItems");
+
--- JS/product.js
+++ JS/product.js
@@ -1,7 +1,7 @@
 //For filter option
 const categories = document.querySelectorAll(".category");
-const products = document.querySelectorAll(".card");
-const brands=document.querySelectorAll(".brand");
+const products = document.querySelectorAll(".product-card");
+const brands = document.querySelectorAll(".brand");
 
 //category
 categories.forEach((category) => {
@@ -10,53 +10,44 @@ categories.forEach((category) => {
 
 //brand
 brands.forEach((brand) => {
-    brand.addEventListener("change", filterProducts)
-})
+  brand.addEventListener("change", filterProducts);
+});
 
 function filterProducts() {
   const selectedCategories = []; //list to store products based on categories
   const selectedBrands = []; //list to store products based on brands
-  
+
   //to get product by categories
   categories.forEach((category) => {
     if (category.checked) {
-        selectedCategories.push(category.value);
+      selectedCategories.push(category.value);
     }
   });
 
   //to get product by brand
   brands.forEach((brand) => {
-    if(brand.checked){
-        selectedBrands.push(brand.value);
+    if (brand.checked) {
+      selectedBrands.push(brand.value);
     }
   });
 
   //filter products
-products.forEach((product) => {
+  products.forEach((product) => {
     const productCategory = product.getAttribute("data-category");
     const productBrand = product.getAttribute("data-brand");
 
-    const categoryMatch = selectedCategories.length===0 || selectedCategories.includes(productCategory);
-    const brandMatch = selectedBrands.length === 0|| selectedBrands.includes(productBrand);
+    const categoryMatch =
+      selectedCategories.length === 0 ||
+      selectedCategories.includes(productCategory);
+    const brandMatch =
+      selectedBrands.length === 0 || selectedBrands.includes(productBrand);
 
-    if (categoryMatch && brandMatch){
-        product.style.display="block";
+    if (categoryMatch && brandMatch) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
     }
-    else{
-        product.style.display="none";
-    }
-});
+  });
 }
 
-//For adding product to cart
-const card_btns = document.querySelectorAll(".card_btn");
 
-
-card_btns.forEach((card_btn)=>{
-    card_btn.addEventListener("click", addProduct);
-    // localStorage.setItem("cart_item");
-}) 
-
-function addProduct(){
-    console.log("hi");
-}
